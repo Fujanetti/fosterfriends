c2a42e710cce16557b776d99a1f099939d7424c3
Jordan Deren <jordanderen1@gmail.com>
2021-02-23 12:31:48 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :pets, only: %w[index show]
+  resources :shelters, only: %w[show] do
+    resources :pets, only: %w[new edit update destroy update]
+  end
+  resources :pet_reviews, only: %w[new create]
+  resources :dashboard, only: %w[index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
