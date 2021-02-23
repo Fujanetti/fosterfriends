8522452aa825874614d6b08cbc01c5e8b0c21bf7
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-02-23 12:00:38 +0000

    Shelter and Pet model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_110733) do
+ActiveRecord::Schema.define(version: 2021_02_23_115739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.bigint "shelter_id", null: false
+    t.string "animal_type"
+    t.float "price_per_day"
+    t.string "gender"
+    t.string "breed"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "shelters", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shelters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +56,6 @@ ActiveRecord::Schema.define(version: 2021_02_23_110733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "shelters"
+  add_foreign_key "shelters", "users"
 end
