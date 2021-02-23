b561a0b53d8cc6c575ee1241c889c1d5b5186eea
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-02-23 12:12:40 +0000

    ShelterReview model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_120809) do
+ActiveRecord::Schema.define(version: 2021_02_23_121124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 2021_02_23_120809) do
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
+  create_table "shelter_reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "shelter_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shelter_id"], name: "index_shelter_reviews_on_shelter_id"
+    t.index ["user_id"], name: "index_shelter_reviews_on_user_id"
+  end
+
   create_table "shelters", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
@@ -84,5 +95,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_120809) do
   add_foreign_key "pets", "shelters"
   add_foreign_key "reservations", "pets"
   add_foreign_key "reservations", "users"
+  add_foreign_key "shelter_reviews", "shelters"
+  add_foreign_key "shelter_reviews", "users"
   add_foreign_key "shelters", "users"
 end
