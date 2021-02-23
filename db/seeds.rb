0973218f9044c5fdea9eb12acbb32cefa30e2fb2
gianluca galota <gianluca.galota@gmail.com>
2021-02-23 16:05:13 +0000

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,35 +9,64 @@
 puts "Cleaning database..."
 
 Pet.destroy_all
+User.destroy_all
+Shelter.destroy_all
+
+jerry = User.create!(
+  first_name: "Jerry", 
+  last_name: "Deren", 
+  email: "jerryderry@aol.com", 
+  user_type: "caretaker", 
+  address: "31 Taco Bell", 
+  city: "Lisbon", 
+  zip_code: 10965, 
+  password: "123456", 
+  birth_date: Date.today
+)
+
+  owner = User.create!(
+    first_name: "John", 
+    last_name: "Jones", 
+    email: "hallal@aol.com", 
+    user_type: "owner", 
+    address: "14 Brighton", 
+    city: "Lisbon", 
+    zip_code: 10965, 
+    password: "123456", 
+    birth_date: Date.today
+  )
+
+puts "Creating shelter..."
+
+shelter_one = Shelter.create!(
+  name: "shelter one",
+  user: User.last
+)
+
 
 puts "Creating pets..."
 
-jerry_the_dog = {
-    name: FAKER::Creature::Dog.name,
+jerry_the_dog = Pet.create!(
+    name: Faker::Creature::Dog.name,
     age: Faker::Number.between(from: 1, to: 10),
-    shelter_id: 1,
+    shelter: Shelter.first,
     animal_type: Faker::Creature::Animal.name,
-    price_per_day: Faker::Creature::Animal.name,
+    price_per_day: 55,
     gender: Faker::Creature::Dog.gender,
     breed: Faker::Creature::Dog.breed,
     description: Faker::Creature::Dog.meme_phrase
-}
+)
 
-giura_the_dog = {
-  name: FAKER::Creature::Dog.name,
+giura_the_dog = Pet.create!(
+  name: Faker::Creature::Dog.name,
   age: Faker::Number.between(from: 1, to: 10),
-  shelter_id: 2,
+  shelter: Shelter.first,
   animal_type: Faker::Creature::Animal.name,
-  price_per_day: Faker::Creature::Animal.name,
+  price_per_day: 100,
   gender: Faker::Creature::Dog.gender,
   breed: Faker::Creature::Dog.breed,
   description: Faker::Creature::Dog.meme_phrase
-}
+)
 
-
-[jerry_the_dog, giura_the_dog].each do |attributes|
-  pet = Pet.create!(attributes)
-  puts "Created #{pet.name}"
-end
 
 puts "Finished!"
