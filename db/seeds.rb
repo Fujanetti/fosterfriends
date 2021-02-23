502eda94a4b818b353c9f73422a8a26b3cc86c84
gianluca galota <gianluca.galota@gmail.com>
2021-02-23 13:25:38 +0000

    seeds created need to be tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Pet.destroy_all
+
+puts "Creating pets..."
+
+10.times do
+  pet = Pet.new(
+    name: FAKER::Creature::Dog.name,
+    age: Faker::Number.between(from: 1, to: 10),
+    animal_type: Faker::Creature::Animal.name,
+    price_per_day: Faker::Creature::Animal.name,
+    gender: Faker::Creature::Dog.gender,
+    breed: Faker::Creature::Dog.breed,
+    description: Faker::Creature::Dog.meme_phrase
+  )
+  pet.save!
+end
+
+puts "#{pet}"
