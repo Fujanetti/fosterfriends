7268f408a80ba32a0d408afc2cd64fe31164c945
gianluca galota <gianluca.galota@gmail.com>
2021-02-25 11:30:25 +0000

    working on review create

--- app/controllers/pet_reviews_controller.rb
+++ app/controllers/pet_reviews_controller.rb
@@ -1,18 +1,21 @@
 class PetReviewsController < ApplicationController
 
   def new
-    @pet = Pet.find(params[:pet_id])
+    # @pet = Pet.find(params[:pet_id])
+    @reservation = Reservation.find(params[:reservation_id])
     @pet_review = PetReview.new
   end
 
   def create
     @pet_review = PetReview.new(pet_review_params)
-    @pet = Pet.find(params[:pet_id])
+    @reservation = Reservation.find(params[:reservation_id])
+    @pet = @reservation.pet
     @user = current_user
     @pet_review.pet = @pet
-    @pet_review.save
-
-    redirect_to pet_path(@pet)
+    @pet_review.user = @user
+    
+    redirect_to reservations_path
+   
   end
 
   private
