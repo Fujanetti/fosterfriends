ad40f506ceafbb0bfd181d3cce84b2e623a659c1
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-02-25 12:57:03 +0000

    Photo functionality added with cloudinary

--- app/models/pet_review.rb
+++ app/models/pet_review.rb
@@ -1,4 +1,6 @@
 class PetReview < ApplicationRecord
   belongs_to :pet
   belongs_to :user
+
+  has_many_attached :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   CATEGORIES = ['Caretaker', 'Shelter']
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :shelter, dependent: :destroy
   has_many :pets, through: :shelter, dependent: :destroy
+
+  has_one_attached :photo
 
   validates :first_name, presence: true
   validates :last_name, presence: true
