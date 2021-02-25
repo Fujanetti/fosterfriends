b92555aa8d86d26ac85155955b184ff656499aeb
Thales Gomes <thalessud@gmail.com>
2021-02-25 14:41:31 +0000

    new comments

--- app/javascript/components/init_booking_pet_price.js
+++ app/javascript/components/init_booking_pet_price.js
@@ -8,29 +8,23 @@ const initBookingPetPrice = () => {
 
     const bookingForm = document.querySelector('#new_reservation')
 
-    bookingForm.addEventListener('change', (event) => {    // Calculates total price, only after picking dates
+    bookingForm.addEventListener('change', (event) => {  // Calculates total price, only after picking dates
       const price = +bookingPetPrice.dataset.petPrice;
-      console.log(price);
       const startYear = document.querySelector('#reservation_start_date_1i').value;
       const startMoth = document.querySelector('#reservation_start_date_2i').value;
       const startDay = document.querySelector('#reservation_start_date_3i').value;
       const endYear = document.querySelector('#reservation_end_date_1i').value;
       const endMoth = document.querySelector('#reservation_end_date_2i').value;
       const endDay = document.querySelector('#reservation_end_date_3i').value;
-      console.log(startYear)
 
       const startDate = Date.parse(`${startYear}/${startMoth}/${startDay}`);
       const endDate = Date.parse(`${endYear}/${endMoth}/${endDay}`);
       const totalDays = (endDate - startDate) / (1000 * 60 * 60 * 24); // Convert from milliseconds to days.
 
-      console.log(startDate)
-      console.log(endDate)
-      console.log(totalDays)
       const totalPrice = totalDays * price;
-      console.log(totalPrice);
-      displayPrice.innerText = `Total: ${totalPrice}$`
+      displayPrice.innerText = `Total: ${totalPrice}$` // Display the price on reservation/new.
       const reservationPrice = document.querySelector('#reservation_total_price');
-      reservationPrice.value = totalPrice;
+      reservationPrice.value = totalPrice; // Sets Reservation instance price.
 
   })
 
