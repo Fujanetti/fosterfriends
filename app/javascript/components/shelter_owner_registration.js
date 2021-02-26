16c2022cac09519058b8ca92fa5f4f1df5db1cf9
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-02-26 08:36:21 +0000

    java script file created

---
+++ app/javascript/components/shelter_owner_registration.js
@@ -0,0 +1,6 @@
+const createShelter = () => {
+  const userType = document.querySelectory("#user_user_type");
+  console.log(userType);
+}
+
+export { createShelter }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,6 +30,8 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initBookingPetPrice } from '../components/init_booking_pet_price';
 
+import { createShelter } from '../components/shelter_owner_registration';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
